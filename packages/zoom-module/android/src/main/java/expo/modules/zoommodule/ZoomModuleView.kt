395e7cc41de327c9d63a089ce90abb0d68cfcef8
Shawna Donnelly <shawleigh17@gmail.com>
2024-06-08 18:12:56 -0600

    update files for zoom module

---
+++ packages/zoom-module/android/src/main/java/expo/modules/zoommodule/ZoomModuleView.kt
@@ -0,0 +1,7 @@
+package expo.modules.zoommodule
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ZoomModuleView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
---
+++ packages/zoom-module/src/index.ts
@@ -0,0 +1,18 @@
+import { EventEmitter, requireNativeModule } from "expo-modules-core";
+
+// Import the native module. On web, it will be resolved to ZoomModule.web.ts
+// and on native platforms to ZoomModule.ts
+import ZoomModule from "./ZoomModule";
+
+// Get the native constant value.
+export const PI = ZoomModule.PI;
+
+export function hello(): string {
+  return ZoomModule.hello();
+}
+
+export async function setValueAsync(value: string) {
+  return await ZoomModule.setValueAsync(value);
+}
+
+const emitter = new EventEmitter(ZoomModule ?? requireNativeModule(ZoomModule));
